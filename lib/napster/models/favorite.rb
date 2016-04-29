ba354de9c5ce6978261ab1f847bc44c55f2be59c
Jason Kim <iamjsonkim@gmail.com>
2016-04-29 16:37:03 -0700

    Feature favorite model (#17)
    * feature: favorite model
    
    * remove the comment

---
+++ lib/napster/models/favorite.rb
@@ -0,0 +1,54 @@
+module Napster
+  module Models
+    # Favorite model
+    class Favorite
+      ATTRIBUTES = [:type,
+                    :id,
+                    :name,
+                    :modified,
+                    :href,
+                    :privacy,
+                    :images,
+                    :description,
+                    :favorite_count,
+                    :free_play_compliant].freeze
+
+      ATTRIBUTES.each do |attribute|
+        attr_accessor attribute
+      end
+
+      attr_accessor :client
+
+      def initialize(arg)
+        @client = arg[:client] if arg[:client]
+        return unless arg[:data]
+
+        ATTRIBUTES.each do |attribute|
+          send("#{attribute}=", arg[:data][attribute.to_s.camel_case_lower])
+        end
+      end
+
+      def self.collection(arg)
+        arg[:data].map do |favorite|
+          Favorite.new(data: favorite, client: @client)
+        end
+      end
+
+      # Top level methods
+
+      def members_who_favorited_albums(id)
+        e = 'Invalid playlist id'
+        raise ArgumentError, e unless Napster::Moniker.check(id, :album)
+        response = @client.get("/albums/#{id}/favorited/members")
+        Member.collection(data: response['members'], client: @client)
+      end
+
+      def members_who_favorited_artists(id)
+        e = 'Invalid playlist id'
+        raise ArgumentError, e unless Napster::Moniker.check(id, :artist)
+        response = @client.get("/artists/#{id}/favorited/members")
+        Member.collection(data: response['members'], client: @client)
+      end
+    end
+  end
+end
---
+++ spec/lib/napster/models/favorite_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+fixture = FixtureLoader.init('main.json')
+config_hash = ConfigLoader.init
+config_variables = config_hash['config_variables']
+options = {
+  api_key: config_variables['API_KEY'],
+  api_secret: config_variables['API_SECRET']
+}
+client = Napster::Client.new(options)
+album_id = fixture['album']['id']
+artist_id = fixture['artist']['id']
+
+describe Napster::Models::Favorite do
+  it 'has a class' do
+    expect(Napster::Models::Favorite).not_to be nil
+  end
+
+  describe '.new' do
+    it 'should instantiate without data' do
+      tag = Napster::Models::Favorite.new({})
+
+      expect(tag.class).to eql(Napster::Models::Favorite)
+    end
+
+    it 'should instantiate with a client' do
+      tag = Napster::Models::Favorite.new(client: client)
+
+      expect(tag.class).to eql(Napster::Models::Favorite)
+    end
+  end
+
+  it '#members_who_favorited_albums' do
+    members = client.favorites.members_who_favorited_albums(album_id)
+    expect(members.class).to eql(Array)
+    expect(members.first.class).to eql(Napster::Models::Member)
+  end
+
+  it '#members_who_favorited_artists' do
+    members = client.favorites.members_who_favorited_artists(artist_id)
+    expect(members.class).to eql(Array)
+    expect(members.first.class).to eql(Napster::Models::Member)
+  end
+end
